5e461a2ddaa2d836bb88a26b82f26ff64d2cdfa0
adrianpalacio <145379361+adrianpalacio@users.noreply.github.com>
2024-01-29 14:33:46 +0000

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
-package org.example;
+package software.ulpgc.kata4;
+
+import java.sql.*;
+import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+        try (Connection connection = DriverManager.getConnection(urlOf("chinook.db"))) {
+            TrackLoader trackLoader = new SqliteTrackLoader(connection);
+            List<Track> tracks = trackLoader.loadAll();
+            for (Track track : tracks) {
+                System.out.println(track);
+            }
+        }
     }
+
+    private static String urlOf(String filename) {
+        return "jdbc:sqlite:" + filename;
+    }
+
+
 }
+
